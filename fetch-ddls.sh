4312a8bdd04317d29b94488966f2e5dd6146faac
R C <oyisre@outlook.com>
2024-10-25 19:35:28 +0000

    Fix CDM schema url

--- fetch-ddls.sh
+++ fetch-ddls.sh
@@ -1,17 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-SCHEMA="public"
+set -euxo pipefail
+
+# Set current directory to that of the script, for consistent file IO
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$SCRIPT_DIR"
+
+SCHEMA_NAME="public"
 
 cd database/ddl
-curl -s \
-  https://raw.githubusercontent.com/OHDSI/CommonDataModel/main/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_ddl.sql \
-  | sed "s/@cdmDatabaseSchema/${SCHEMA}/g" > 1OMOPCDM_postgresql_5.4_ddl.sql
-curl -s \
-  https://raw.githubusercontent.com/OHDSI/CommonDataModel/main/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_primary_keys.sql \
-  | sed "s/@cdmDatabaseSchema/${SCHEMA}/g" > 2OMOPCDM_postgresql_5.4_primary_keys.sql
-curl -s \
-  https://raw.githubusercontent.com/OHDSI/CommonDataModel/main/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_indices.sql \
-  | sed "s/@cdmDatabaseSchema/${SCHEMA}/g" > 3OMOPCDM_postgresql_5.4_indices.sql
-curl -s \
-  https://raw.githubusercontent.com/OHDSI/CommonDataModel/main/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_constraints.sql \
-  | sed "s/@cdmDatabaseSchema/${SCHEMA}/g" > 4OMOPCDM_postgresql_5.4_constraints.sql
+curl -L \
+  https://github.com/OHDSI/CommonDataModel/raw/refs/tags/v5.4.2/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_ddl.sql \
+  | sed "s/@cdmDatabaseSchema/${SCHEMA_NAME}/g" > 1_OMOPCDM_postgresql_5.4_ddl.sql
+curl -L \
+  https://github.com/OHDSI/CommonDataModel/raw/refs/tags/v5.4.2/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_primary_keys.sql \
+  | sed "s/@cdmDatabaseSchema/${SCHEMA_NAME}/g" > 2_OMOPCDM_postgresql_5.4_primary_keys.sql
+curl -L \
+  https://github.com/OHDSI/CommonDataModel/raw/refs/tags/v5.4.2/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_indices.sql \
+  | sed "s/@cdmDatabaseSchema/${SCHEMA_NAME}/g" > 3_OMOPCDM_postgresql_5.4_indices.sql
+curl -L \
+  https://github.com/OHDSI/CommonDataModel/raw/refs/tags/v5.4.2/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_constraints.sql \
+  | sed "s/@cdmDatabaseSchema/${SCHEMA_NAME}/g" > 4_OMOPCDM_postgresql_5.4_constraints.sql
