0e855a3f1509ade10ac756e148833b87f5193fb3
Raghavan C <7621312+oyisre@users.noreply.github.com>
2025-01-29 19:37:23 +0000

    Download correct version of wasm sqlite as referenced in index.html

--- notebooks/fetch-latest-sqlite-wasm.sh
+++ notebooks/fetch-latest-sqlite-wasm.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 pushd public
-curl -LJO https://github.com/sql-js/sql.js/releases/download/v1.12.0/sqljs-wasm.zip
-unzip sqljs-wasm.zip
-rm sqljs-wasm.zip
+curl -LJO https://github.com/sql-js/sql.js/releases/download/v1.12.0/sqljs-all.zip
+unzip sqljs-all.zip 'sql-wasm-debug.*'
+rm sqljs-all.zip
 popd
