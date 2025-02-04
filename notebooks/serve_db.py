a9f56648c30427bf22da3c0b12bdc6f6cc9ecfcb
Raghavan C <7621312+oyisre@users.noreply.github.com>
2025-02-04 13:02:27 +0000

    Add link to Datasette app

--- notebooks/serve_db.py
+++ notebooks/serve_db.py
@@ -10,9 +10,13 @@ class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
+    def end_headers(self):
+        self.send_header("Access-Control-Allow-Origin", "*")
+        super().end_headers()
 
 with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
     print(f"Serving directory '{DIRECTORY}' at http://localhost:{PORT}")
+    print(f"Try https://lite.datasette.io/?url=http://localhost:8000/sdc_cdm.db for a friendlier GUI into the database")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
