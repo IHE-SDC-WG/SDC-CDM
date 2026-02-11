e680bf759fd8ac0693d5bfe69a96935fc9435a3b
Josh Liben <josh@topology.health>
2026-02-11 16:01:39 -0500

    added query for sifting NAACRR DAta Dict for Table extensions

---
+++ database/ddl/sqlserver/NAACR_DATA_DICT_SEARCH.sql
@@ -0,0 +1,19 @@
+SELECT [concept_id]
+      , [concept_name]
+      , [vocabulary_id]
+      , [concept_class_id]
+      , [concept_code]
+      , [domain_id]
+      , [valid_start_date]
+      , [valid_end_date]
+FROM [NAACCRDDPRD].[dbo].[CONCEPT]
+WHERE valid_end_date > GETDATE()
+
+-- AND
+-- (
+--        [vocabulary_id] LIKE '%DEMOGRAPHIC%' OR
+--        [vocabulary_id] = 'FOLLOW_UP_RECURRENCE' OR
+--        [vocabulary_id] LIKE '%CONFIDENTIAL%' OR
+--        [vocabulary_id] = 'RECORD_ID'
+--   )
+ORDER BY [vocabulary_id], [concept_class_id]
